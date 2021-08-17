456a45e113346a7cfd49e2fd23a1b21c91a86715
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-17 15:18:31 +0300

    Added waiting for db test cases

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,29 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """
+            Tests waiting for database when database is available
+        """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
+            get_item.return_value = True
+            call_command('wait_for_db')
+
+            self.assertEqual(get_item.call_count, 1)
+    
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, time_sleep):
+        """
+            Tests waiting for database
+        """
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
+            get_item.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+
+            self.assertEqual(get_item.call_count, 6)
