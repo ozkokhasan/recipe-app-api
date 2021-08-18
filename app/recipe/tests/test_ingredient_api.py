f7c9defe3ba57807fac62a4a3e76dcf43485a4e7
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-18 18:22:33 +0300

    Added listing ingredient feature

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import AppRegistryNotReady
-from django.http import response
 from django.urls import reverse
 from django.test import TestCase
 
