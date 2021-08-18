cde02ebbd4acfce6f42dae660cbad6c7d9fc893c
Hasan Özkök <hasan.ozkok@akinon.com>
2021-08-18 09:31:36 +0300

    added manage user and test cases

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -13,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('email', 'password', 'name')
         extra_kwargs = {
-            'password':{
+            'password': {
                 'write_only': True,
                 'min_length': 5,
                 'style': {
@@ -27,6 +27,18 @@ class UserSerializer(serializers.ModelSerializer):
             Create a new user with encrypted password and return it
         """
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+            Update a user, setting the password correctly and return it
+        """
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
